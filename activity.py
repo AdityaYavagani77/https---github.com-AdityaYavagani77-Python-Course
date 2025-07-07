1a45d3720b485419d022d9f5a8059b3f40cd0678
Aditya <adithunderbolt071@gmail.com>
2025-07-07 19:42:00 +1000

    did lesson2

---
+++ activity.py
@@ -0,0 +1,16 @@
+print("My name is Aditya")
+
+# Coding is fun
+a = "aditya"
+print("my name length in number of chars", len(a))
+b = 1
+
+c = 3
+d = b + c 
+print(d)
+print(a)
+'''
+Python is powerful
+and is used by many people
+
+'''
---
+++ lesson2.py
@@ -0,0 +1,5 @@
+#This is a simple sentence for printing
+print("I am learning python \ncode")
+
+#This is basic math
+print(29+67)
