cc5315bcfe6b2499608b45e7287fbe3b775f6676
Aditya <adithunderbolt071@gmail.com>
2025-07-14 19:34:10 +1000

    added lesson3

---
+++ lesson3.py
@@ -0,0 +1,18 @@
+'''
+a = input("What is your favourite colour?")
+
+print(type(a))
+
+b = input("Enter your Height")
+print(type(b))
+'''
+
+age = 12
+name = "Aditya"
+height = 153.9
+s = True
+
+print(type(age))
+print(type(name))
+print(type(height))
+print(type(s))
