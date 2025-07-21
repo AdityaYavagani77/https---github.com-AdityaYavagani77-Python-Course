96f4f630c1edcd65f87728c3a362964d6349f32d
Aditya <adithunderbolt071@gmail.com>
2025-07-21 19:51:45 +1000

    did activity 3

--- lesson4.py
+++ lesson4.py
@@ -2,7 +2,7 @@
 sum = 98+94+41+96+11
 
 print("The averge height of the trees is ",sum/5)
-"""
+
 Amount = int(input("Please enter an amount to withdraw ?"))
 
 bill_100 = Amount//100
@@ -19,3 +19,6 @@ print(bill_50)
 print(bill_20)
 print(bill_10)
 print(Amount)
+"""
+sum = 40+70+50+60
+print(sum/4)
