77f50094ee8b40c73666eeedd079469d7fa5f7cd
Aditya <adithunderbolt071@gmail.com>
2025-07-27 20:59:10 +1000

    Added homework 2 and modified it

---
+++ homework2.py
@@ -0,0 +1,8 @@
+
+Birthday_1 = "Thomas is born on the 19th of September 2011"
+Birthday_2 = "Ethan is born on the 8th of March 1999"
+Birthday_3 = "Aditya is born on the 18th of December 2012"
+Birthday_4 = "John is born on the 31st of January 1967"
+Birthday_5 = "Aahil is born on the 22nd of April 2009."
+
+print(" My name is Raj and I have lots of friends,", Birthday_1, ",", Birthday_2, ",", Birthday_3, ",", Birthday_4, ",", Birthday_5) 
