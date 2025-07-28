a3e59d5d7cef39ecce341836688349ac36ac52f9
Aditya <adithunderbolt071@gmail.com>
2025-07-28 19:36:54 +1000

    did activity 2

--- lesson5.py
+++ lesson5.py
@@ -1,6 +1,18 @@
+"""
 num = int(input("Please enter a positive or negtive number:"))
 
 if num > 0:
    print("Your number is postive")
 else:
    print("Your number is negitive ")
+"""
+item = str(input("What item are you buying?"))
+price = int(input("What is the cost of your item?"))
+selling = int(input("What price are you selling it for?"))
+
+if selling > price:
+    print("You are making a profit!")
+    print( "Your profit is",selling-price)
+else:
+    print("You are getting a loss of money?")
+    print("Your loss is", price-selling)
