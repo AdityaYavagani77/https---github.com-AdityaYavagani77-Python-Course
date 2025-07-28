16e2a74b594378ad77f90a28672fc99aca28444f
Aditya <adithunderbolt071@gmail.com>
2025-07-28 18:13:32 +1000

    edited hw4

--- homework4.py
+++ homework4.py
@@ -1,4 +1,4 @@
-user_input_string = input("Enter a whole number to find its square root: ")
-number = int(user_input_string)
-square_root_result = number ** 0.5
+user_input = input("Enter a whole number to find its square root: ")
+number = int(user_input)
+square_root_result = int(number ** 0.5)
 print(f"The square root of {number} is: {square_root_result}")
