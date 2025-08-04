2118644b2d6f2a78cfe03265371aff3e10d3ae16
Aditya <adithunderbolt071@gmail.com>
2025-08-04 19:30:47 +1000

    did activty 1

---
+++ lesson6.py
@@ -0,0 +1,13 @@
+num_1 = int(input("Please enter a number:"))
+num_2 = int(input("Please enter a number:"))
+num_3 = int(input("Please enter any number:"))
+
+if num_1 and num_2 and num_3:
+    print("All the numbers are True")
+else:
+    print("At least one of the numbers is False")
+
+if num_3 < 6 or num_2 < 6 or num_1 < 6:
+    print("At least one of he numbers is less than 5 or 5")
+else:
+    print("The numbers are equal to or greater than 6")
