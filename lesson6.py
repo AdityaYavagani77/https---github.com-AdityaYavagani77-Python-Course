8af48623c228923c86c913a4263773ff822a50d8
Aditya <adithunderbolt071@gmail.com>
2025-08-04 19:34:50 +1000

    did activity2

--- lesson6.py
+++ lesson6.py
@@ -1,3 +1,4 @@
+"""
 num_1 = int(input("Please enter a number:"))
 num_2 = int(input("Please enter a number:"))
 num_3 = int(input("Please enter any number:"))
@@ -11,3 +12,11 @@ if num_3 < 6 or num_2 < 6 or num_1 < 6:
     print("At least one of he numbers is less than 5 or 5")
 else:
     print("The numbers are equal to or greater than 6")
+"""
+num_1 = int(input("Please enter a number:"))
+num_2 = int(input("Please enter a number:"))
+
+if not (num_1==num_2):
+    print("These 2 numbers are not equal")
+else:
+    print("The numbers are equal")
