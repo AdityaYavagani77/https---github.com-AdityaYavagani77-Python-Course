28a2c06b4ceb030c260c352cf7f2b8f8f54f1ffe
Aditya <adithunderbolt071@gmail.com>
2025-08-04 19:47:22 +1000

    Did activity 3

--- lesson6.py
+++ lesson6.py
@@ -12,7 +12,7 @@ if num_3 < 6 or num_2 < 6 or num_1 < 6:
     print("At least one of he numbers is less than 5 or 5")
 else:
     print("The numbers are equal to or greater than 6")
-"""
+
 num_1 = int(input("Please enter a number:"))
 num_2 = int(input("Please enter a number:"))
 
@@ -20,3 +20,17 @@ if not (num_1==num_2):
     print("These 2 numbers are not equal")
 else:
     print("The numbers are equal")
+"""
+
+w = float(input("Please enter your weight:"))
+h = float(input("Please enter your height:"))
+
+BMI = w/(h/100)**2
+print("Your BMI is", BMI)
+
+if BMI <= 18.4:
+    print("You are underweight")
+elif BMI <= 24.9:
+    print("You're healthy")
+else:
+    print("You are obese")
