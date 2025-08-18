7023246223a6fd3675cda49bb9a94572fc67c523
Aditya <adithunderbolt071@gmail.com>
2025-08-18 19:32:16 +1000

    did activity 2

--- lesson8.py
+++ lesson8.py
@@ -1,5 +1,20 @@
+"""
 a = 2
 b = 6
 h = 7
 
 print(b+h**a)
+"""
+
+a = int(input("Please enter a number:"))
+b = int(input("Please enter another number:"))
+
+if a%b == 0:
+    print("a is divisible by b")
+else:
+    print("a is not divisible by b")
+
+if b%a == 0:
+    print("b is divisible by a")
+else:
+    print("b is not divisible by a")
