82810cf94943e1aa3d8991244b3f89196747a7fc
Aditya <adithunderbolt071@gmail.com>
2025-08-25 19:39:01 +1000

    did activity 2

--- lesson9.py
+++ lesson9.py
@@ -1,4 +1,4 @@
-
+"""
 q1 = str(input("Please enter your gender:"))
 q2 = int(input("What is your age:"))
 if q1 == "male":
@@ -8,3 +8,15 @@ if q1 == "male":
         print("You are male and under 18")
 else:
     print("You are female")
+"""
+Medical_cause = input("Do you have a medical cause:")
+
+if Medical_cause == "yes":
+    print("You are allowed to appear!")
+
+elif Medical_cause == "no":
+    Attendance = float(input("Put in your attendance percentage:"))
+    if Attendance >= 75:
+        print("you are allowed to appear!")
+    else:
+        print("you are not allowed to appear")
