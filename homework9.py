d783a16be53bba9cd9638d4844af5925cd38b174
Aditya <adithunderbolt071@gmail.com>
2025-08-25 20:03:29 +1000

    finished homework 9

---
+++ homework9.py
@@ -0,0 +1,9 @@
+
+user_input = input("Please enter your age:")
+
+age = "10""11""12""13""14""15""16""17""18""19""20"
+
+if user_input in age:
+    print("Your age is or in between 10 and 20")
+else:
+    print("You are are younger than 10 or older than 20")
