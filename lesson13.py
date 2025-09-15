6a069b3e6ec02f9fb62c6c7a147d59f7735ed829
Aditya <adithunderbolt071@gmail.com>
2025-09-15 19:44:17 +1000

    did activty 2

--- lesson13.py
+++ lesson13.py
@@ -1,3 +1,4 @@
+"""
 user_input = input("Please enter a a word:")
 
 charectar = input("Please enter a letter:")
@@ -7,5 +8,15 @@ for i in user_input:
     if i == charectar:
         sum = sum+1
 print(f"The total occarance of the letter {charectar} in {user_input} is {sum}")
+"""
+upper = int(input("Please enter the upper limit:"))
+lower = int(input("Please enter the lower limit:"))
 
-    
+for i in range(lower,upper):
+    if i > 1:
+        for anything in range(2, i):
+            if i % anything == 0:
+
+                break
+            else:
+                print(i)
