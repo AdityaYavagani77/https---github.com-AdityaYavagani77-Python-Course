cb5ff9d0879b3b64c7f243f46f82710c88258adc
Aditya <adithunderbolt071@gmail.com>
2025-10-06 20:18:02 +1100

    did 2 activities

---
+++ lesson16.py
@@ -0,0 +1,19 @@
+"""
+def sports():
+    print("Cricket Rules!")
+    print("Soccer Forever!")
+    print("Basketball is the Best!")
+
+sports()
+sports()
+sports()
+sports()
+
+def weather_functions():
+    print("Summer is very very hot.")
+    print("Winter is ver very cold.")
+    print("Autumn is dry and mildly cold.")
+    print("Spring is pleasent and has lots of flowers.")
+
+weather_functions()
+"""
