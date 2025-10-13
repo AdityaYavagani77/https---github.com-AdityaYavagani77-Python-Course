647f0e8fc185ab2b46cb45981358af79ba190f83
Aditya <adithunderbolt071@gmail.com>
2025-10-13 20:41:45 +1100

    did activty 2

--- lesson17.py
+++ lesson17.py
@@ -1,4 +1,4 @@
-
+"""
 due_amount = int(input("Please enter the amount that you are supposed to pay:"))
 tip_amount = int(input("Also, please enter the percentage you want to tip:"))
 
@@ -8,3 +8,17 @@ def tip_calculator(total,percentage):
 tip = tip_calculator(due_amount,tip_amount)
 print(f"Your total tip is {tip}")
 print(f"Your total expenditure is {tip + due_amount}")
+"""
+def cube(number):
+    return(number*number*number)
+
+def check_system(number):
+    if (number%3 == 0):
+        return cube(number)
+    else:
+        return "That the number is not divisible by 3 so I did not calculate the cube!"
+
+
+real_number = int(input("Please enter a number to be cubed:"))
+
+print(check_system(real_number))
