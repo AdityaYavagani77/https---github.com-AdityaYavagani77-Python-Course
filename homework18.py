ef66fbca89efabdce794bce067226af789fc6a44
Aditya <adithunderbolt071@gmail.com>
2025-11-03 19:43:45 +1100

    did homework 18

---
+++ homework18.py
@@ -0,0 +1,9 @@
+due_amount = int(input("Please enter the amount that you are supposed to pay:"))
+tip_amount = int(input("Also, please enter the percentage you want to tip:"))
+
+def tip_calculator(total,percentage):
+    return(total*percentage)/100
+
+tip = tip_calculator(due_amount,tip_amount)
+print(f"Your total tip is {tip}")
+print(f"Your total expenditure is {tip + due_amount}")
