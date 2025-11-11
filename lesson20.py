704761ca7011d3ab14ec9e46ff6e35f2630341e7
Aditya <adithunderbolt071@gmail.com>
2025-11-11 19:35:08 +1100

    did second act

--- lesson20.py
+++ lesson20.py
@@ -1,3 +1,4 @@
+"""
 import random
 
 playing = True
@@ -15,3 +16,25 @@ while playing:
 
     else:
         print("Your guess isn't quite right, please try agian! \n")
+        """
+import random
+
+
+userr_choice = input("Choose rock, paper, or scissors: ").lower()
+
+choices = ['rock', 'paper', 'scissors']
+computer_choice = random.choice(choices)
+
+
+print(f"\nYou chose: {userr_choice}")
+print(f"The computer chose: {computer_choice}\n")
+
+
+if userr_choice == computer_choice:
+    print("It's a tie!")
+elif (userr_choice == 'rock' and computer_choice == 'scissors') or \
+     (userr_choice == 'paper' and computer_choice == 'rock') or \
+     (userr_choice == 'scissors' and computer_choice == 'paper'):
+    print("You win!")
+else:
+    print("The computer wins!") 
