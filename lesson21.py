f2a53766c6bdc8b0bafa09335ab1c082a1c40046
Aditya <adithunderbolt071@gmail.com>
2025-11-17 20:39:43 +1100

    did activty 3

--- lesson21.py
+++ lesson21.py
@@ -13,7 +13,7 @@ print(f"\n Current date and time is {now}")
 
 print(today.month)
 print(today.year)
-"""
+
 from datetime import datetime as DT
 
 Time = DT.now()
@@ -23,3 +23,18 @@ print("Minute =", Time.minute)
 print("Second =", Time.second)
 print("Microsecond =", Time.microsecond)
 
+
+import calendar
+
+yy = 1452
+mm = 4
+
+print(calendar.month(yy,mm))
+"""
+
+import calendar
+
+cal = calendar.Calendar()
+
+for x in cal.itermonthdates(1333, 8):
+    print(x)
