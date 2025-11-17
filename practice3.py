c044c29158dd5bf853d9b30d495be544f36c5609
Aditya <adithunderbolt071@gmail.com>
2025-11-17 20:27:38 +1100

    did activty 2

--- practice3.py
+++ practice3.py
@@ -1,21 +1,21 @@
 import random
 
 
-user_choice = input("Choose rock, paper, or scissors: ").lower()
+userr_choice = input("Choose rock, paper, or scissors: ").lower()
 
 choices = ['rock', 'paper', 'scissors']
 computer_choice = random.choice(choices)
 
 
-print(f"\nYou chose: {user_choice}")
+print(f"\nYou chose: {userr_choice}")
 print(f"The computer chose: {computer_choice}\n")
 
 
-if user_choice == computer_choice:
+if userr_choice == computer_choice:
     print("It's a tie!")
-elif (user_choice == 'rock' and computer_choice == 'scissors') or \
-     (user_choice == 'paper' and computer_choice == 'rock') or \
-     (user_choice == 'scissors' and computer_choice == 'paper'):
+elif (userr_choice == 'rock' and computer_choice == 'scissors') or \
+     (userr_choice == 'paper' and computer_choice == 'rock') or \
+     (userr_choice == 'scissors' and computer_choice == 'paper'):
     print("You win!")
 else:
-    print("The computer wins!")
+    print("The computer wins!") 
