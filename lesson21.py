31c70ba58d4a5f86470ac914eb588464556d3e38
Aditya <adithunderbolt071@gmail.com>
2025-11-17 20:13:41 +1100

    did 1st activity

---
+++ lesson21.py
@@ -0,0 +1,14 @@
+import datetime
+
+from datetime import date
+
+today = date.today()
+
+now = datetime.datetime.now()
+
+print(f"\n Today's date is {today} ")
+
+print(f"\n Current date and time is {now}")
+
+print(today.month)
+print(today.year)
