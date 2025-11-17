3c537a626ccad2d4e0b32a458c06f117c533001e
Aditya <adithunderbolt071@gmail.com>
2025-11-17 20:28:00 +1100

    duidi activty 2

--- lesson21.py
+++ lesson21.py
@@ -1,3 +1,4 @@
+"""
 import datetime
 
 from datetime import date
@@ -12,3 +13,13 @@ print(f"\n Current date and time is {now}")
 
 print(today.month)
 print(today.year)
+"""
+from datetime import datetime as DT
+
+Time = DT.now()
+
+print("Hour =", Time.hour)
+print("Minute =", Time.minute)
+print("Second =", Time.second)
+print("Microsecond =", Time.microsecond)
+
