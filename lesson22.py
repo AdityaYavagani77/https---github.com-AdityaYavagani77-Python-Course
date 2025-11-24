e1808eef3dcd7f8f24ff9c4b75bf1705a3ac98a5
Aditya <adithunderbolt071@gmail.com>
2025-11-24 20:41:21 +1100

    did second activity

--- lesson22.py
+++ lesson22.py
@@ -1,3 +1,4 @@
+"""
 empty_list = []
 print()
 
@@ -10,3 +11,18 @@ print(triples)
 aList = [100,200,300,400,500]
 aList = aList[ : : -1]
 print(aList,"\n")
+"""
+
+def match_words(words):
+    ctr = 0
+    lst = []
+
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            ctr += 1
+            lst.append(word)
+    print("List of words with first and last letter same\n", lst)
+    return ctr
+
+count = match_words(['abc','cfc','xyz','aba','1221'])
+print("Number of words having first and last letters same:",count)
