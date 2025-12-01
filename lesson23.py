fb3cf9d61317ffa4871c822b2f857b073d4d8b1c
Aditya <adithunderbolt071@gmail.com>
2025-12-01 20:35:20 +1100

    Did last activty

--- lesson23.py
+++ lesson23.py
@@ -12,7 +12,7 @@ tuplex3 = (2,4,3,5,4,6,7,8,6,1)
 slice = tuplex3[:6]
 
 print(slice)
-"""
+
 
 def palind(r):
     e = len(r)-1
@@ -32,4 +32,20 @@ if (palind(r)):
 else:
     print("The Tuple is not a Flip-Flop")
 
-    
+    """
+
+weather = (1,0,0,0,1,1,0)
+sunny = 0
+rainy = 0
+
+for i in range(0,7):
+    if (weather[i]==0):
+        rainy+=1
+    else:
+        sunny+=1
+
+if (sunny>rainy):
+    print("Good Weather")
+
+else:
+    print("Bad Weather")
